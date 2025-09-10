674ea5b5c0bcbb90b3cad164c984af05415d56b2
Aanjaneya24 <pandeyaanjaneya76@gmail.com>
2025-09-11 00:34:28 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import Appnavbar from './components/AppNavbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ItemModal from './components/ItemModal';
+import {Provider}from 'react-redux';
+import { Container } from 'reactstrap';
+import store from './store';
+import ShoppingList from './components/ShoppingList';
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <Appnavbar />
+          <Container>
+          <ItemModal />
+
+          <ShoppingList />
+          </Container>
+        </div>
+      </Provider>
+    );
+  }
+}
 export default App;
